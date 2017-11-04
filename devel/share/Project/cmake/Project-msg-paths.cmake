cec75a38066da26189d0ac4a65e1461472b2cc76
Zihao <zzh394608263@gmail.com>
2017-11-04 10:38:20 -0500

    -a

---
+++ devel/share/Project/cmake/Project-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(Project_MSG_INCLUDE_DIRS "/home/zihao/catkin_ws/src/beginner_tutorials/msg")
+set(Project_MSG_DEPENDENCIES std_msgs)
